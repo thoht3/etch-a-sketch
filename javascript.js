c73c7c27231d80c59859c0fd2a9b025575e6c758
thoht3 <tho.hotrong@gmail.com>
2024-04-23 09:57:29 +0700

    tao xong bai etch sketch goi ne

--- javascript.js
+++ javascript.js
@@ -1,13 +1,24 @@
 let div = document.createElement("div");
 
 let gridContainer = document.getElementById("gridContainer");
+let gridNumber = prompt("Mày muốn bao nhiêu ô?", 0);
+if (gridNumber > 100) {
+    gridNumber = prompt("Nhiều quá, tối đa 100 thôi má. Nhập lại nè!");
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+    for (let i = 0; i < gridNumber; i++) {
+        for (let j = 0; j < gridNumber; j++) {
             let div = document.createElement("div");
             div.classList.add("grid-item");
             gridContainer.appendChild(div);
+            div.addEventListener("mouseover",  (eve) => {
+                eve.target.style.backgroundColor = "#ff0000";
+            });
+            div.addEventListener("mouseleave",  (eve) => {
+                eve.target.style.backgroundColor = "blue";
+            });
         }
     }
 });
+
